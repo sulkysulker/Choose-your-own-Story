9001ba68a5ae03b2154f370e9ad8b5a50c10056a
sulkysulker <tippanawaryash19@gmail.com>
2026-01-10 11:11:55 +0530

    initial project steup

---
+++ backend/core/config.py
@@ -0,0 +1,10 @@
+from typing import List
+from pydantic_settings import BaseSettings
+from pydantic import field_validator
+
+class Settings(BaseSettings):
+    API_PREFIX:str="/api"
+    DEBUG:bool=False
+    DATABASE_URL:str
+    ALLOWED_ORIGINS:str=""
+    OPENAI_API_KEY:str
