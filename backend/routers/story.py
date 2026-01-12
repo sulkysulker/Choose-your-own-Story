4d0675aa932f76c768e3d7382526097ec7a096fc
sulkysulker <tippanawaryash19@gmail.com>
2026-01-12 21:47:21 +0530

    added the story generator using llm

--- backend/routers/story.py
+++ backend/routers/story.py
@@ -8,6 +8,7 @@ from models.story import Story,StoryNode
 from models.job import StoryJob
 from schemas.story import CompleteStoryResponse,CompleteStoryNodeResponse,CreateStoryRequest
 from schemas.job import StoryJobResponse
+from core.story_generator import StoryGenerator
 
 router=APIRouter(
     prefix="/stories",
@@ -57,9 +58,8 @@ def generate_story_task(job_id:str,theme:str,session_id:str):
             job.status="processing"
             db.commit()
 
-            story={}
-
-            job.story_id=1
+            story=StoryGenerator.generate_story(db,session_id,theme)
+            job.story_id=story.id
             job.status="completed"
             job.completed_at=datetime.now()
             db.commit()
@@ -85,4 +85,30 @@ def get_complete_story(story_id:int,db:Session=Depends(get_db)):
     return complete_story
 
 def build_complete_story_tree(db:Session,story:Story) -> CompleteStoryResponse:
-    pass
+    nodes=db.query(StoryNode).filter(StoryNode.id == story.id).all()
+
+    node_dict={}
+    for node in nodes:
+        node_response=CompleteStoryNodeResponse(
+            id=node.id,
+            content=node.content,
+            is_ending=node.is_ending,
+            is_winning_ending=node.is_winning_ending,
+            options=node.options
+        )
+
+        node_dict[node.id]=node_response
+
+    root_node=next((node for node in nodes if node.is_root),None)
+
+    if not root_node:
+        raise HTTPException(status_code=500,detail="Story root node not found/some error occurred")
+
+    return CompleteStoryResponse(
+        id=story.id,
+        title=story.title,
+        session_id=story.session_id,
+        created_at=story.created_at,
+        root_node=node_dict[root_node.id],
+        all_nodes=node_dict
+    )
